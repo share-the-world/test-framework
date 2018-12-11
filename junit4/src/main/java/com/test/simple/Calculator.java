a83c3ea6a0bf556177faac3069c3de78a04f359e
shiyuan2he <shiyuan4work@126.com>
2018-12-11 14:13:36 +0800

    简单junit4单元测试添加

---
+++ junit4/src/main/java/com/test/simple/Calculator.java
@@ -0,0 +1,15 @@
+package com.test.simple;
+
+/**
+ * @author heshiyuan
+ * @description <p></p>
+ * @path test-framework/com.test.temp
+ * @date 2018-12-11 13:40
+ * Copyright (c) 2018 heshiyuan@huobi.com All rights reserved.
+ */
+public class Calculator {
+
+    public int add(int a, int b){
+        return a+b;
+    }
+}
